b4b769d754b0a344429d2e09afa5b6492e445dfc
YuraS <stallker9696@gmail.com>
2021-01-09 21:24:13 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Image-Gallery
+
+Viewing-Photos is a test task for AgileEngine company.
+
+## Installation
+
+```
+npm install
+npm run start
+```
+
+## Usage
+
+open ```http://localhost:3000``` and see the result.
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Viewing Photos</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
